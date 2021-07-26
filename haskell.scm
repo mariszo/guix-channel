f46c66fed0de214baac4df2dbf315f96e1c613a9
Mariszo <mariszo@itfinger.com>
2021-07-26 14:24:34 +0200

    modify haskell.scm

--- haskell.scm
+++ haskell.scm
@@ -19,40 +19,38 @@
   #:use-module (gnu packages version-control))
 
 (define-public ghc-cabal-install-my
-  (package
-    (name "ghc-cabal-install-my")
-    (version "3.4.0.0")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append
-               "https://hackage.haskell.org/package/cabal-install/cabal-install-"
-               version
-               ".tar.gz"))
-        (sha256
-          (base32
-            "15rylx5pa03jdiwcg1x7zvs6aq3g6phwmi1hz26cl080nczyz00r"))))
-    (build-system haskell-build-system)
-    (inputs
-      `(("ghc-async" ,ghc-async)
-        ("ghc-base16-bytestring" ,ghc-base16-bytestring)
-        ("ghc-cryptohash-sha256" ,ghc-cryptohash-sha256)
-        ("ghc-echo" ,ghc-echo)
-        ("ghc-edit-distance" ,ghc-edit-distance)
-        ("ghc-hashable" ,ghc-hashable)
-        ("ghc-http" ,ghc-http)
-        ("ghc-network-uri" ,ghc-network-uri)
-        ("ghc-random" ,ghc-random)
-        ("ghc-tar" ,ghc-tar)
-        ("ghc-zlib" ,ghc-zlib)
-        ("ghc-hackage-security" ,ghc-hackage-security)
-        ("ghc-regex-base" ,ghc-regex-base)
-        ("ghc-regex-posix" ,ghc-regex-posix)
-        ("ghc-resolv" ,ghc-resolv)
-        ("ghc-lukko" ,ghc-lukko)))
-    (home-page "http://www.haskell.org/cabal/")
-    (synopsis
-      "The command-line interface for Cabal and Hackage.")
-    (description
-      "The \\'cabal\\' command-line program simplifies the process of managing Haskell software by automating the fetching, configuration, compilation and installation of Haskell libraries and programs.")
-    (license license:bsd-3)))
+(package
+  (name "ghc-cabal-install")
+  (version "3.0.0.0")
+  (source
+    (origin
+      (method url-fetch)
+      (uri (string-append
+             "https://hackage.haskell.org/package/cabal-install/cabal-install-"
+             version
+             ".tar.gz"))
+      (sha256
+        (base32
+          "1wda29ifkn50376jidj6ihfk60a64y0bsd7lh3yw15py7a2sfcm4"))))
+  (build-system haskell-build-system)
+  (inputs
+    `(("ghc-async" ,ghc-async)
+      ("ghc-base16-bytestring" ,ghc-base16-bytestring)
+      ("ghc-cryptohash-sha256" ,ghc-cryptohash-sha256)
+      ("ghc-echo" ,ghc-echo)
+      ("ghc-edit-distance" ,ghc-edit-distance)
+      ("ghc-hashable" ,ghc-hashable)
+      ("ghc-http" ,ghc-http)
+      ("ghc-network-uri" ,ghc-network-uri)
+      ("ghc-network" ,ghc-network)
+      ("ghc-random" ,ghc-random)
+      ("ghc-tar" ,ghc-tar)
+      ("ghc-zlib" ,ghc-zlib)
+      ("ghc-hackage-security" ,ghc-hackage-security)
+      ("ghc-resolv" ,ghc-resolv)))
+  (home-page "http://www.haskell.org/cabal/")
+  (synopsis
+    "The command-line interface for Cabal and Hackage.")
+  (description
+    "The \\'cabal\\' command-line program simplifies the process of managing Haskell software by automating the fetching, configuration, compilation and installation of Haskell libraries and programs.")
+  (license license:bsd-3)))
